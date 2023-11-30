dc1f77e9c89cbed7190e0c1ff54ff1cbfbc54a13
Ziyoviddin Mamadosimov <univerziyo@gmail.com>
2023-11-30 15:25:28 +0400

    Created model Item

--- store/item/models.py
+++ store/item/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
